246fff4a86e7aa7184da5c3ba8120517c1399a7c
Maurice Laveaux <m.laveaux@tue.nl>
2025-02-10 14:07:15 +0100

    Removed unused code from the xtask

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -8,11 +8,7 @@
 use std::env;
 use std::error::Error;
 use std::process::ExitCode;
-use std::str::FromStr;
 
-use benchmark::Rewriter;
-
-mod benchmark;
 mod coverage;
 mod sanitizer;
 
@@ -26,28 +22,6 @@ fn main() -> Result<ExitCode, Box<dyn Error>> {
     let task = args.next();
 
     match task.as_deref() {
-        Some("benchmark") => {
-            if let Some(rewriter) = args.next() {
-                // Use the upstream mcrl2
-                if let Some(output_path) = args.next() {
-                    benchmark::benchmark(output_path, Rewriter::from_str(&rewriter)?)?
-                } else {
-                    println!("Missing argument for output file");
-                    return Ok(ExitCode::FAILURE);
-                }
-            } else {
-                println!("Missing argument for rewriter");
-                return Ok(ExitCode::FAILURE);
-            }
-        }
-        Some("create-table") => {
-            if let Some(input_path) = args.next() {
-                benchmark::create_table(input_path)?;
-            } else {
-                println!("Missing argument for input file");
-                return Ok(ExitCode::FAILURE);
-            }
-        }
         Some("coverage") => {
             // Take the other parameters for cargo.
             let other_arguments: Vec<String> = args.collect();
