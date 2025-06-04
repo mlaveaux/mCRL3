2b429ff312b4fb17e4cd854ec8f8fc4061fdc6e0
Maurice Laveaux <mauricelaveaux@gmail.com>
2025-06-04 22:45:37 +0200

    Improved parse error messages, and fixed some issues with the grammar

--- crates/syntax/src/parse.rs
+++ crates/syntax/src/parse.rs
@@ -20,11 +20,12 @@ impl UntypedProcessSpecification {
     pub fn parse(spec: &str) -> Result<UntypedProcessSpecification, MCRL3Error> {
         pest::set_error_detail(true);
 
-        let mut result = Mcrl2Parser::parse(Rule::MCRL2Spec, spec)?;
-        let root = result.next().ok_or("Could not parse mCRL2 specification")?;
+        let mut result = Mcrl2Parser::parse(Rule::MCRL2Spec, spec)
+            .map_err(|err| extend_parser_error(err))?;
+        let root = result.next().expect("Could not parse mCRL2 specification");
         trace!("Parse tree {}", DisplayPair(root.clone()));
 
-        Mcrl2Parser::MCRL2Spec(ParseNode::new(root)).map_err(|err| extend_parser_error(err).into())
+        Ok(Mcrl2Parser::MCRL2Spec(ParseNode::new(root))?)
     }
 }
 
@@ -33,11 +34,12 @@ impl UntypedDataSpecification {
     pub fn parse(spec: &str) -> Result<UntypedDataSpecification, MCRL3Error> {
         pest::set_error_detail(true);
 
-        let mut result = Mcrl2Parser::parse(Rule::DataSpec, spec)?;
-        let root = result.next().ok_or("Could not parse mCRL2 data specification")?;
+        let mut result = Mcrl2Parser::parse(Rule::DataSpec, spec)
+            .map_err(|err| extend_parser_error(err))?;
+        let root = result.next().expect("Could not parse mCRL2 data specification");
         trace!("Parse tree {}", DisplayPair(root.clone()));
 
-        Mcrl2Parser::DataSpec(ParseNode::new(root)).map_err(|err| extend_parser_error(err).into())
+        Ok(Mcrl2Parser::DataSpec(ParseNode::new(root))?)
     }
 }
 
@@ -45,23 +47,50 @@ impl UntypedStateFrmSpec {
     pub fn parse(spec: &str) -> Result<UntypedStateFrmSpec, MCRL3Error> {
         pest::set_error_detail(true);
 
-        let mut result = Mcrl2Parser::parse(Rule::StateFrmSpec, spec)?;
+        let mut result = Mcrl2Parser::parse(Rule::StateFrmSpec, spec)
+            .map_err(|err| extend_parser_error(err))?;
         let root = result
             .next()
-            .ok_or("Could not parse mCRL2 state formula specification")?;
+            .expect("Could not parse mCRL2 state formula specification");
         trace!("Parse tree {}", DisplayPair(root.clone()));
 
-        Mcrl2Parser::StateFrmSpec(ParseNode::new(root)).map_err(|err| extend_parser_error(err).into())
+        Ok(Mcrl2Parser::StateFrmSpec(ParseNode::new(root))?)
     }
 }
 
 fn extend_parser_error(error: Error<Rule>) -> Error<Rule> {
     error.renamed_rules(|rule| match rule {
-        Rule::DataExprAdd => "+".to_string(),
-        Rule::DataExprWhr => "whr".to_string(),
+        Rule::DataExprWhr => "DataExpr whr AssignmentList end".to_string(),
+        Rule::DataExprUpdate => "DataExpr[(DataExpr -> DataExpr)*]".to_string(),
+        Rule::DataExprApplication => "DataExpr(DataExpr*)".to_string(),
+
+        // DataExpr Binary Operators
         Rule::DataExprConj => format!("{}", DataExprBinaryOp::Conj),
         Rule::DataExprDisj => format!("{}", DataExprBinaryOp::Disj),
         Rule::DataExprImpl => format!("{}", DataExprBinaryOp::Implies),
+        Rule::DataExprEq => format!("{}", DataExprBinaryOp::Equal),
+        Rule::DataExprNeq => format!("{}", DataExprBinaryOp::NotEqual),
+        Rule::DataExprLess => format!("{}", DataExprBinaryOp::LessThan),
+        Rule::DataExprLeq => format!("{}", DataExprBinaryOp::LessEqual),
+        Rule::DataExprGreater => format!("{}", DataExprBinaryOp::GreaterThan),
+        Rule::DataExprGeq => format!("{}", DataExprBinaryOp::GreaterEqual),
+        Rule::DataExprIn => format!("{}", DataExprBinaryOp::In),
+        Rule::DataExprDiv => format!("{}", DataExprBinaryOp::Div),
+        Rule::DataExprIntDiv => format!("{}", DataExprBinaryOp::IntDiv),
+        Rule::DataExprMod => format!("{}", DataExprBinaryOp::Mod),
+        Rule::DataExprMult => format!("{}", DataExprBinaryOp::Multiply),
+        Rule::DataExprAdd => format!("{}", DataExprBinaryOp::Add),
+        Rule::DataExprSubtract => format!("{}", DataExprBinaryOp::Subtract),
+        Rule::DataExprAt => format!("{}", DataExprBinaryOp::At),
+        Rule::DataExprCons => format!("{}", DataExprBinaryOp::Cons),
+        Rule::DataExprSnoc => format!("{}", DataExprBinaryOp::Snoc),
+        Rule::DataExprConcat => format!("{}", DataExprBinaryOp::Concat),
+
+        // Regular Formulas
+        Rule::RegFrmAlternative => "RegFrm + RegFrm".to_string(),
+        Rule::RegFrmComposition => "RegFrm . RegFrm".to_string(),
+        Rule::RegFrmIteration => "RegFrm*".to_string(),
+        Rule::RegFrmPlus => "RegFrm+".to_string(),
         _ => format!("{:?}", rule),
     })
 }
