88be998e0b12373577b9803984a33e8ad9ac9a31
Maurice Laveaux <mauricelaveaux@gmail.com>
2025-06-14 16:07:17 +0200

    Added mimalloc as al alternative to jemalloc

--- crates/utilities/src/global_allocator.rs
+++ crates/utilities/src/global_allocator.rs
@@ -8,6 +8,11 @@ static GLOBAL_ALLOCATOR: counting_allocator::CountingAllocator = counting_alloca
 #[global_allocator]
 static GLOBAL_ALLOCATOR: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
+#[cfg(not(feature = "mcrl3_measure-allocs"))]
+#[cfg(feature = "mcrl3_mimalloc")]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: mimalloc::MiMalloc = mimalloc:: MiMalloc;
+
 pub fn print_stats() -> usize {
     #[cfg(feature = "mcrl3_measure-allocs")]
     {
