9542f864883e62299d60c9751c21311094d5d097
Maurice Laveaux <mauricelaveaux@gmail.com>
2025-07-02 19:50:46 +0200

    Continued the implementation of the block allocator.

--- crates/unsafety/src/block_allocator.rs
+++ crates/unsafety/src/block_allocator.rs
@@ -1,6 +1,7 @@
-use std::{alloc::Layout, array, mem::ManuallyDrop, ptr::{self, NonNull}, sync::Mutex};
+use std::{alloc::Layout, array, fmt, mem::ManuallyDrop, ptr::{self, NonNull}, sync::Mutex};
 
 use allocator_api2::alloc::{AllocError, Allocator};
+use itertools::Itertools;
 
 
 /// This is a slab allocator or also called block allocator for a concrete type `T`. It stores blocks of `Size` to minimize the overhead of individual memory allocations (which are typically in the range of one or two words).
@@ -29,34 +30,49 @@ impl<T, const N: usize> BlockAllocator<T, N> {
     /// Similar to the [Allocator] trait, but instead of passing a layout we allocate just an object of type `T`.
     pub fn allocate_object(&mut self) -> Result<NonNull<T>, AllocError> {
         if let Some(free) = self.free {
-
-
-
-            // let result = free.cast::<T>();
-            self.free = Some(free.next)
-
+            unsafe {
+                // Safety: By invariant of the freelist the next must point to the next free element.
+                self.free = Some(free.as_ref().next);
+            }
+            return Ok(free.cast::<T>());
         }
 
-        match &mut self.head_block {
+        let block = match &mut self.head_block {
             Some(block) => {
                 if block.is_full() {
                     let mut new_block = Box::new(Block::new());
                     std::mem::swap(block, &mut new_block);   
-                    block.next = Some(new_block);             
+                    block.next = Some(new_block);
                 }
+
+                block
             },
             None => {
-                self.head_block = Some(Box::new(Block::new()));
+                let block = Box::new(Block::new());
+                self.head_block = Some(block);
+                self.head_block.as_mut().expect("Is initialized in the previous line")
             }
-        }
+        };
         
         // After this the first block definitely has space
-        self.head_block.
+        let length = block.length;
+        block.length += 1;
+        unsafe {
+            // Safety: We take a pointer (value does not have to be initialized) to a ManuallDrop<T>, which has the same layout as T.
+            Ok(NonNull::new_unchecked(&mut block.data[length].data as *mut ManuallyDrop<T> as *mut T))
+        }
     }
 
     /// Deallocate the given pointer.
     pub fn deallocate_object(&mut self, _ptr: NonNull<T>) {
 
+    }
+
+    /// Returns an iterator over the free list entries.
+    fn iter_free(&self) -> FreeListIterator<T> {
+        FreeListIterator {
+            current: self.free,
+        }
     }
 }
 
@@ -84,9 +100,11 @@ unsafe impl<T, const N: usize> Allocator for AllocBlock<T, N> {
     }
 }
 
+
+
 union Entry<T> {
     data: ManuallyDrop<T>,
-    next: NonNull<Entry<T>, 
+    next: NonNull<Entry<T>>,
 }
 
 ///
@@ -100,7 +118,7 @@ struct Block<T, const N: usize> {
 impl<T, const N: usize> Block<T, N> {
     fn new() -> Self {
         Self {
-            data: array::from_fn(|_i| Entry { next: ptr::dangling() }),
+            data: array::from_fn(|_i| Entry { next: NonNull::dangling() }),
             length: 0,
             next: None,
         }
@@ -108,7 +126,7 @@ impl<T, const N: usize> Block<T, N> {
 
     fn with_next(next: Box<Block<T, N>>) -> Self {
         Self {
-            data: array::from_fn(|_i| Entry { next: ptr::dangling() }),
+            data: array::from_fn(|_i| Entry { next: NonNull::dangling() }),
             length: 0,
             next: Some(next),
         }
@@ -120,6 +138,32 @@ impl<T, const N: usize> Block<T, N> {
     }
 }
 
+/// Iterator over the free list entries in a BlockAllocator.
+struct FreeListIterator<T> {
+    current: Option<NonNull<Entry<T>>>,
+}
+
+impl<T> Iterator for FreeListIterator<T> {
+    type Item = NonNull<Entry<T>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(current) = self.current {
+            // Safety: We assume the free list is properly constructed and current points to a valid Entry
+            unsafe {
+                self.current = Some(current.as_ref().next);
+            }
+            Some(current)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T, const N: usize> fmt::Debug for BlockAllocator<T, N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "freelist = {:?}", self.iter_free().format(", "))
+    }
+}
 
 #[cfg(test)]
 mod tests {
