a016ddda58ce6ecb37bb1709b8245d2e649e69d7
Maurice Laveaux <m.laveaux@tue.nl>
2025-07-08 00:33:05 +0200

    Extended the implementation of the RecursiveLock

--- crates/sharedmutex/src/recursive_lock.rs
+++ crates/sharedmutex/src/recursive_lock.rs
@@ -1,40 +1,59 @@
-use std::{cell::Cell, error::Error, mem, ops::Deref};
+use std::cell::Cell;
+use std::error::Error;
+use std::mem;
+use std::ops::Deref;
 
-use crate::{BfSharedMutex, BfSharedMutexWriteGuard};
-
-
+use crate::BfSharedMutex;
+use crate::BfSharedMutexReadGuard;
+use crate::BfSharedMutexWriteGuard;
 
 /// An extension of the `SharedMutex` that allows recursive read locking without deadlocks.
-struct RecursiveLock<T> {
-    mutex: BfSharedMutex<T>,
+pub struct RecursiveLock<T> {
+    inner: BfSharedMutex<T>,
 
     /// The number of times the current thread has read locked the mutex.
     recursive_depth: Cell<usize>,
 }
 
 impl<T> RecursiveLock<T> {
-    /// The same as `BfSharedMutex::write`.
-    fn write<'a>(&'a self) -> Result<BfSharedMutexWriteGuard<'a, T>, Box<dyn Error + 'a>> {
-        self.mutex.write()
+    /// Creates a new `RecursiveLock` with the given data.
+    pub fn new(data: T) -> Self {
+        RecursiveLock {
+            inner: BfSharedMutex::new(data),
+            recursive_depth: Cell::new(0),
+        }
+    }
+
+    /// Creates a new `RecursiveLock` from an existing `BfSharedMutex`.
+    pub fn from_mutex(mutex: BfSharedMutex<T>) -> Self {
+        RecursiveLock {
+            inner: mutex,
+            recursive_depth: Cell::new(0),
+        }
+    }
+
+    delegate::delegate! {
+        to self.inner {
+            pub fn data_ptr(&self) -> *const T;
+            pub fn is_locked(&self) -> bool;
+            pub fn is_locked_exclusive(&self) -> bool;
+            pub fn write(&self) -> Result<BfSharedMutexWriteGuard<'_, T>, Box<dyn Error + '_>>;
+            pub fn read(&self) -> Result<BfSharedMutexReadGuard<'_, T>, Box<dyn Error + '_>>;
+        }
     }
 
     /// Acquires a read lock on the mutex, allowing for recursive read locking.
-    fn read_recursive<'a>(&'a self) -> Result<RecursiveLockReadGuard<'a, T>, Box<dyn Error + 'a>> {
+    pub fn read_recursive<'a>(&'a self) -> Result<RecursiveLockReadGuard<'a, T>, Box<dyn Error + 'a>> {
         if self.recursive_depth.get() == 0 {
             // If we are not already holding a read lock, we acquire one.
             // Acquire the read guard, but forget it to prevent it from being dropped.
-            mem::forget(self.mutex.read());
-            Ok(RecursiveLockReadGuard {
-                mutex: self,
-            })
+            mem::forget(self.inner.read());
+            Ok(RecursiveLockReadGuard { mutex: self })
         } else {
             // If we are already holding a read lock, we just increment the depth.
             self.recursive_depth.set(self.recursive_depth.get() + 1);
-            Ok(RecursiveLockReadGuard {
-                mutex: self,
-            })
+            Ok(RecursiveLockReadGuard { mutex: self })
         }
-        
     }
 }
 
@@ -50,8 +69,11 @@ impl<T> Deref for RecursiveLockReadGuard<'_, T> {
     fn deref(&self) -> &Self::Target {
         // There can only be shared guards, which only provide immutable access to the object.
         unsafe {
-            debug_assert!(!self.mutex.mutex.data_ptr().is_null(), "Data pointer should not be null");
-            self.mutex.mutex.data_ptr().as_ref().unwrap_unchecked()
+            debug_assert!(
+                !self.mutex.inner.data_ptr().is_null(),
+                "Data pointer should not be null"
+            );
+            self.mutex.inner.data_ptr().as_ref().unwrap_unchecked()
         }
     }
 }
@@ -63,9 +85,8 @@ impl<T> Drop for RecursiveLockReadGuard<'_, T> {
             // If we are not holding a read lock anymore, we release the mutex.
             // This will allow other threads to acquire a read lock.
             unsafe {
-                self.mutex.mutex.create_read_guard_unchecked();
+                self.mutex.inner.create_read_guard_unchecked();
             }
         }
     }
 }
-
