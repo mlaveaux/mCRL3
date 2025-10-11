d31b208896960a26969b461365b9d133a79fbf24
Maurice Laveaux <m.laveaux@tue.nl>
2025-10-11 12:27:59 +0200

    Renumber before inserting into the hashtable, to improve performance

--- crates/reduction/src/signature_refinement.rs
+++ crates/reduction/src/signature_refinement.rs
@@ -248,28 +248,25 @@ where
             partition.partition_marked_with(block_index, &mut split_builder, |state_index, partition| {
                 signature(state_index, partition, &state_to_key, &mut builder);
 
+
                 // Compute the signature of a single state
-                let index = if let Some((_, index)) = id.get_key_value(&Signature::new(&builder)) {
-                    // Keep track of the index for every signature, either use the arena to allocate space or simply borrow the value.
-                    state_to_key[state_index] = *index;
-                    *index
-                } else {
-                    let slice = arena.alloc_slice_copy(&builder);
-
-                    let number = if let Some(key) = renumber(&builder, &key_to_signature) {
-                        key
+               let index = if let Some(key) = renumber(&builder, &key_to_signature) {
+                    key
+               } else {
+                    if let Some((_, index)) = id.get_key_value(&Signature::new(&builder)) {
+                        state_to_key[state_index] = *index;
+                        *index
                     } else {
-                        let result = key_to_signature.len();
+                        let slice = arena.alloc_slice_copy(&builder);
+                        let number = BlockIndex::new(key_to_signature.len());
+                        id.insert(Signature::new(slice), number);
                         key_to_signature.push(Signature::new(slice));
-                        BlockIndex::new(result)
-                    };
 
-                    id.insert(Signature::new(slice), number);
+                        // (branching) Keep track of the signature for every block in the next partition.
+                        state_to_key[state_index] = number;
 
-                    // (branching)  Keep track of the signature for every block in the next partition.
-                    state_to_key[state_index] = number;
-
-                    number
+                        number
+                    }
                 };
 
                 trace!("State {state_index} signature {builder:?} index {index}");
