1981351cedb9609a75851c87932c06594a3c449b
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-06 21:54:53 +0100

    Made ATermString a proper aterm

--- tools/mcrl2/crates/mcrl2/src/atermpp/aterm_string.rs
+++ tools/mcrl2/crates/mcrl2/src/atermpp/aterm_string.rs
@@ -1,31 +1,39 @@
 use std::fmt;
 
-use crate::ATerm;
+use mcrl2_macros::mcrl2_derive_terms;
 
-/// Represents an atermpp::aterm_string from the mCRL2 toolset.
-#[derive(PartialEq, Eq)]
-pub struct ATermString {
-    term: ATerm,
+use crate::ATermRef;
+
+pub fn is_aterm_string(term: &ATermRef<'_>) -> bool {
+    term.get_head_symbol().arity() == 0
 }
 
-impl ATermString {
-    /// Creates a new `ATermString` from the given term.
-    pub fn new(term: ATerm) -> Self {
-        Self { term }
+#[mcrl2_derive_terms]
+mod inner {
+    use mcrl2_macros::mcrl2_term;
+
+    use crate::ATerm;
+    use crate::ATermRef;
+    use crate::Markable;
+    use crate::Todo;
+    use crate::is_aterm_string;
+
+    /// Represents an atermpp::aterm_string from the mCRL2 toolset.
+    #[mcrl2_term(is_aterm_string)]
+    pub struct ATermString {
+        term: ATerm,
     }
 
-    /// Returns the string value.
-    pub fn str(&self) -> String {
-        // The Rust::Str should ensure that this is a valid string.
-        self.term.get_head_symbol().name().to_string()
+    impl ATermString {
+        /// Returns the string value.
+        pub fn str(&self) -> String {
+            // The Rust::Str should ensure that this is a valid string.
+            self.term.get_head_symbol().name().to_string()
+        }
     }
 }
 
-impl fmt::Debug for ATermString {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.str())
-    }
-}
+pub use inner::*;
 
 impl fmt::Display for ATermString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
