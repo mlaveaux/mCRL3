a3e2b33a9428f7b53bd653ab268b55769efc6cd7
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-13 13:40:23 +0100

    Use the proper timings for merc-rewrite.

--- tools/rewrite/src/lib.rs
+++ tools/rewrite/src/lib.rs
@@ -1,6 +1,5 @@
 use std::fmt::Debug;
 use std::path::Path;
-use std::time::Instant;
 
 use clap::ValueEnum;
 
@@ -11,6 +10,7 @@ use merc_sabre::NaiveRewriter;
 use merc_sabre::RewriteEngine;
 use merc_sabre::SabreRewriter;
 use merc_utilities::MercError;
+use merc_utilities::Timing;
 
 /// Selects the rewriter to use.
 #[derive(ValueEnum, Debug, Clone)]
@@ -21,16 +21,16 @@ pub enum Rewriter {
 }
 
 /// Rewrites the given REC specification.
-pub fn rewrite_rec(rewriter: Rewriter, filename_specification: &Path, output: bool) -> Result<(), MercError> {
+pub fn rewrite_rec(rewriter: Rewriter, filename_specification: &Path, output: bool, timing: &Timing) -> Result<(), MercError> {
     let (syntax_spec, syntax_terms) = load_rec_from_file(filename_specification)?;
 
     let spec = syntax_spec.to_rewrite_spec();
 
+    let mut rewrite_time = timing.start("rewrite_rec");
     match rewriter {
         Rewriter::Naive => {
             let mut inner = NaiveRewriter::new(&spec);
 
-            let now = Instant::now();
             for term in &syntax_terms {
                 let term = to_untyped_data_expression(term.clone(), None);
                 let result = inner.rewrite(&term);
@@ -38,12 +38,10 @@ pub fn rewrite_rec(rewriter: Rewriter, filename_specification: &Path, output: bo
                     println!("{}", result)
                 }
             }
-            println!("Naive rewrite took {} ms", now.elapsed().as_millis());
         }
         Rewriter::Innermost => {
             let mut inner = InnermostRewriter::new(&spec);
 
-            let now = Instant::now();
             for term in &syntax_terms {
                 let term = to_untyped_data_expression(term.clone(), None);
                 let result = inner.rewrite(&term);
@@ -51,12 +49,10 @@ pub fn rewrite_rec(rewriter: Rewriter, filename_specification: &Path, output: bo
                     println!("{}", result)
                 }
             }
-            println!("Innermost rewrite took {} ms", now.elapsed().as_millis());
         }
         Rewriter::Sabre => {
             let mut sa = SabreRewriter::new(&spec);
 
-            let now = Instant::now();
             for term in &syntax_terms {
                 let term = to_untyped_data_expression(term.clone(), None);
                 let result = sa.rewrite(&term);
@@ -64,9 +60,10 @@ pub fn rewrite_rec(rewriter: Rewriter, filename_specification: &Path, output: bo
                     println!("{}", result)
                 }
             }
-            println!("Sabre rewrite took {} ms", now.elapsed().as_millis());
         }
     }
+
+    rewrite_time.finish();
 
     Ok(())
 }
