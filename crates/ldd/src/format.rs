297cb19dda3da35633ff74f526d05c01f21548f4
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-14 21:42:03 +0100

    Use LddDisplay directly

--- crates/ldd/src/format.rs
+++ crates/ldd/src/format.rs
@@ -1,8 +1,5 @@
 use std::collections::HashSet;
 use std::fmt;
-use std::io;
-use std::io::Write;
-
 use itertools::Itertools;
 
 use crate::Data;
@@ -10,69 +7,80 @@ use crate::Ldd;
 use crate::Storage;
 use crate::iterators::*;
 
-/// Print the vector set represented by the LDD.
-pub fn fmt_node<'a>(storage: &'a Storage, ldd: &Ldd) -> LddDisplay<'a> {
-    LddDisplay {
-        storage,
-        ldd: ldd.clone(),
+/// Helper struct for displaying LDDs in DOT format.
+pub struct LddDot<'a> {
+    storage: &'a Storage,
+    ldd: &'a Ldd,
+}
+
+impl<'a> LddDot<'a> {
+    pub fn new(storage: &'a Storage, ldd: &'a Ldd) -> LddDot<'a> {
+        LddDot {
+            storage,
+            ldd,
+        }
     }
 }
 
-/// Prints the given LDD is the 'dot' format, which can be converted into an image using 'GraphViz'.
-pub fn print_dot(storage: &Storage, f: &mut impl Write, ldd: &Ldd) -> io::Result<()> {
-    write!(
-        f,
-        r#"
-digraph "DD" {{
-graph [dpi = 300];
-center = true;
-edge [dir = forward];
+impl fmt::Display for LddDot<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            r#"
+    digraph "DD" {{
+    graph [dpi = 300];
+    center = true;
+    edge [dir = forward];
 
-"#
-    )?;
+    "#
+        )?;
 
-    // Every node must be printed once, so keep track of already printed ones.
-    #[allow(clippy::mutable_key_type)]
-    let mut marked: HashSet<Ldd> = HashSet::new();
+        // Every node must be printed once, so keep track of already printed ones.
+        #[allow(clippy::mutable_key_type)]
+        let mut marked: HashSet<Ldd> = HashSet::new();
 
-    // We don't show these nodes in the output since every right most node is 'false' and every bottom node is 'true'.
-    // or in our terms empty_set and empty_vector. However, if the LDD itself is 'false' or 'true' we just show the single
-    // node for clarity.
-    if ldd == storage.empty_set() {
-        writeln!(f, "0 [shape=record, label=\"False\"];")?;
-    } else if ldd == storage.empty_vector() {
-        writeln!(f, "1 [shape=record, label=\"True\"];")?;
-    } else {
-        print_node(storage, f, &mut marked, ldd)?;
+        // We don't show these nodes in the output since every right most node is 'false' and every bottom node is 'true'.
+        // or in our terms empty_set and empty_vector. However, if the LDD itself is 'false' or 'true' we just show the single
+        // node for clarity.
+        if self.ldd == self.storage.empty_set() {
+            writeln!(f, "0 [shape=record, label=\"False\"];")?;
+        } else if self.ldd == self.storage.empty_vector() {
+            writeln!(f, "1 [shape=record, label=\"True\"];")?;
+        } else {
+            print_node(self.storage, f, &mut marked, self.ldd)?;
+        }
+
+        writeln!(f, "}}")
     }
-
-    writeln!(f, "}}")
 }
 
+/// Helper struct for displaying LDDs.
 pub struct LddDisplay<'a> {
     storage: &'a Storage,
-    ldd: Ldd,
+    ldd: &'a Ldd,
+}
+
+impl<'a> LddDisplay<'a> {
+    pub fn new(storage: &'a Storage, ldd: &'a Ldd) -> LddDisplay<'a> {
+        LddDisplay {
+            storage,
+            ldd,
+        }
+    }
 }
 
 impl fmt::Display for LddDisplay<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{{")?;
-        print(self.storage, &self.ldd, f)?;
+        writeln!(f, "{{")?;        
+        for vector in iter(self.storage, &self.ldd) {
+            writeln!(f, "[{}]", vector.iter().format(" "))?;
+        }
         write!(f, "}}")
     }
 }
 
-/// Print the vector set represented by the LDD.
-fn print(storage: &Storage, ldd: &Ldd, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    for vector in iter(storage, ldd) {
-        writeln!(f, "[{}]", vector.iter().format(" "))?;
-    }
-
-    Ok(())
-}
-
 #[allow(clippy::mutable_key_type)]
-fn print_node(storage: &Storage, f: &mut impl Write, marked: &mut HashSet<Ldd>, ldd: &Ldd) -> io::Result<()> {
+fn print_node(storage: &Storage, f: &mut fmt::Formatter<'_>, marked: &mut HashSet<Ldd>, ldd: &Ldd) -> fmt::Result {
     if marked.contains(ldd) || ldd == storage.empty_set() || ldd == storage.empty_vector() {
         Ok(())
     } else {
