faa41a4cac1041fbe8fce39966e805f97b76dc0d
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-15 14:39:09 +0100

    Moved the JSON printing for structured logging to the tools crate

--- crates/tools/src/lib.rs
+++ crates/tools/src/lib.rs
@@ -1,7 +1,9 @@
-pub mod console;
-pub mod verbosity;
-pub mod version;
+mod console;
+mod format_key_values;
+mod verbosity;
+mod version;
 
 pub use console::*;
+pub use format_key_values::*;
 pub use verbosity::*;
 pub use version::*;
