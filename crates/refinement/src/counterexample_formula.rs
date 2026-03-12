ba12eca58ca95cc1de018606a8f5a0c29c4ec180
Maurice Laveaux <m.laveaux@tue.nl>
2026-03-12 21:16:25 +0100

    Generate counter examples for stable failures and impossible futures.

--- crates/refinement/src/counterexample_formula.rs
+++ crates/refinement/src/counterexample_formula.rs
@@ -5,19 +5,22 @@ use merc_syntax::ModalityOperator;
 use merc_syntax::MultiAction;
 use merc_syntax::RegFrm;
 use merc_syntax::StateFrm;
+use merc_syntax::StateFrmOp;
 
 /// Represents a counter example.
 pub enum CounterExample<L: TransitionLabel> {
     Trace(Vec<L>),
     WeakTrace(Vec<L>),
+    StableFailures(Vec<L>, L),
+    ImpossibleFutures(Vec<L>, Vec<Vec<L>>),
 }
 
 /// Generates a formula that characterizes the counter example trace.
 pub fn generate_formula<L: TransitionLabel>(counter_example: &CounterExample<L>) -> StateFrm {
-    let mut expr = StateFrm::True;
-
     match counter_example {
         CounterExample::Trace(trace) => {
+            let mut expr = StateFrm::True;
+
             // We build the formula bottom up.
             for label in trace.iter().rev() {
                 expr = StateFrm::Modality {
@@ -26,27 +29,54 @@ pub fn generate_formula<L: TransitionLabel>(counter_example: &CounterExample<L>)
                     expr: Box::new(expr),
                 }
             }
-        }
-        CounterExample::WeakTrace(trace) => {
-            // Build the formula tau*
-            let tau_star = RegFrm::Iteration(Box::new(RegFrm::Action(ActFrm::MultAct(MultiAction::new(vec![])))));
 
-            // We build the formula bottom up: tau* . label ...
-            for label in trace.iter().rev() {
-                expr = StateFrm::Modality {
-                    operator: ModalityOperator::Diamond,
-                    formula: tau_star.clone(),
-                    expr: Box::new(StateFrm::Modality {
-                        operator: ModalityOperator::Diamond,
-                        formula: RegFrm::Action(ActFrm::MultAct(label_to_multi_action(label))),
-                        expr: Box::new(expr),
-                    }),
-                }
-            }
+            expr
+        }
+        CounterExample::WeakTrace(trace) => weaktrace_formula(trace, StateFrm::True, ModalityOperator::Diamond),
+        CounterExample::StableFailures(trace, refusal) => weaktrace_formula(
+            trace,
+            StateFrm::Modality {
+                operator: ModalityOperator::Box,
+                formula: RegFrm::Action(ActFrm::MultAct(label_to_multi_action(refusal))),
+                expr: Box::new(StateFrm::False),
+            },
+            ModalityOperator::Diamond
+        ),
+        CounterExample::ImpossibleFutures(trace, futures) => {
+            let expressions = futures.iter().map(|future| {
+                weaktrace_formula(future, StateFrm::False, ModalityOperator::Box)
+            }).collect::<Vec<_>>();
+
+            // Generate a conjunction of the expressions for each future.
+            let expr = expressions.into_iter().fold(StateFrm::True, |acc, expr| {
+                StateFrm::Binary { op: StateFrmOp::Conjunction, lhs: Box::new(acc), rhs: Box::new(expr) }
+            });
+
+            weaktrace_formula(trace, expr, ModalityOperator::Diamond)
+        },
+    }
+}
+
+/// Generates a formula [tau* . label1 . tau* . label2 ...]expr that characterizes the weak trace counter example.
+fn weaktrace_formula<L: TransitionLabel>(trace: &Vec<L>, expr: StateFrm, modality: ModalityOperator) -> StateFrm {
+    // Build the formula tau*
+    let tau_star = RegFrm::Iteration(Box::new(RegFrm::Action(ActFrm::MultAct(MultiAction::new(vec![])))));
+
+    // We build the formula bottom up: tau* . label ...
+    let mut result = expr;
+    for label in trace.iter().rev() {
+        result = StateFrm::Modality {
+            operator: modality,
+            formula: tau_star.clone(),
+            expr: Box::new(StateFrm::Modality {
+                operator: modality,
+                formula: RegFrm::Action(ActFrm::MultAct(label_to_multi_action(label))),
+                expr: Box::new(result),
+            }),
         }
     }
 
-    expr
+    result
 }
 
 /// Converts a label to a multi-action, where tau labels are converted to the empty multi-action.
