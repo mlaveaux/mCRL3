3adf6c69a060a367bcf29447d592324524c1d84e
Maurice Laveaux <m.laveaux@tue.nl>
2026-03-14 12:03:10 +0100

    Added the lib.rs for the typecheck crate

---
+++ crates/typecheck/src/lib.rs
@@ -0,0 +1,15 @@
+mod alias;
+mod data_specification;
+mod is_finite;
+mod is_well_typed;
+mod name_resolution;
+mod non_empty;
+mod standard_sorts;
+
+pub use alias::*;
+pub use data_specification::*;
+pub use is_finite::*;
+pub use is_well_typed::*;
+pub use name_resolution::*;
+pub use non_empty::*;
+pub use standard_sorts::*;
