7341f9000f2c427e3786973b9d1e6a370b8d434d
Maurice Laveaux <m.laveaux@tue.nl>
2026-03-23 19:03:27 +0100

    Removed the annotations in several other places.

--- crates/aterm/src/storage/aterm_storage.rs
+++ crates/aterm/src/storage/aterm_storage.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
 use std::hash::Hash;
+use std::ptr::NonNull;
+use std::ptr::slice_from_raw_parts_mut;
 
 use equivalent::Equivalent;
 use merc_unsafety::AllocBlock;
@@ -14,7 +16,7 @@ use crate::storage::SharedTerm;
 
 /// Storage for ATerms with a fixed number of arguments.
 ///
-/// Should be the same layout as `SharedTerm`.
+/// Should be the same layout as [`crate::SharedTerm`] for the shared fields.
 #[repr(C)]
 #[derive(Hash, Eq, PartialEq)]
 struct SharedTermFixed<const N: usize> {
@@ -24,7 +26,7 @@ struct SharedTermFixed<const N: usize> {
 
 /// Storage for ATerms with a fixed number of arguments.
 ///
-/// Should be the same layout as `SharedTerm`.
+/// Should be the same layout as [`crate::SharedTerm`] for the shared fields.
 #[repr(C)]
 #[derive(Hash, Eq, PartialEq)]
 pub(crate) struct SharedTermInt {
@@ -32,13 +34,28 @@ pub(crate) struct SharedTermInt {
     args: [usize; 1],
 }
 
+impl SharedTermInt {
+    /// Returns the value of the integer term.
+    pub fn value(&self) -> usize {
+        self.args[0]
+    }
+}
+
+/// The actualy storage for ATerm. Terms are stored in separated
+/// `StablePointerSet`s based on their arity, and whether they have annotations
+/// or not.
+/// 
+/// 
 pub(crate) struct ATermStorage {
+    /// Stores terms of any arity.
     terms: StablePointerSet<SharedTerm>,
 
-    int_terms: StablePointerSet<SharedTermFixed<1>, FxBuildHasher, AllocBlock<SharedTermFixed<1>, 1024>>,
+    /// Stores the fixed size integer terms
+    int_terms: StablePointerSet<SharedTermInt, FxBuildHasher, AllocBlock<SharedTermInt, 1024>>,
 }
 
 impl ATermStorage {
+    /// Creates a new, empty storage.
     pub fn new() -> Self {
         Self {
             terms: StablePointerSet::new(),
@@ -51,6 +68,7 @@ impl ATermStorage {
         self.int_terms.len() + self.terms.len()
     }
 
+    /// Retains only the terms for which the given predicate returns `true`.
     pub fn retain<F>(&self, mut f: F)
     where
         F: FnMut(&StablePointer<SharedTerm>) -> bool,
@@ -59,6 +77,10 @@ impl ATermStorage {
         self.terms.retain(|term| f(term));
     }
 
+    /// Inserts a term into the storage, returning a pointer to the stored term
+    /// and whether it was newly inserted.
+    ///
+    /// The given term should not be an integer term.
     pub unsafe fn insert_equiv_dst<'a, Q, C>(
         &self,
         value: &'a Q,
@@ -70,5 +92,24 @@ impl ATermStorage {
         C: Fn(*mut SharedTerm, &'a Q),
     {
         unsafe { self.terms.insert_equiv_dst(value, length, construct) }
+    }
+
+    /// Inserts an integer term into the storage, returning a pointer to the stored term
+    /// and whether it was newly inserted.
+    pub unsafe fn insert_int_term<'a>(
+        &self,
+        symbol: SymbolRef<'_>,
+        value: usize,
+    ) -> (StablePointer<SharedTerm>, bool) {
+        unsafe {
+            let (result, inserted) = self.int_terms.insert(SharedTermInt {
+                symbol: std::mem::transmute::<SymbolRef<'_>, SymbolRef<'static>>(symbol),
+                args: [value as usize],
+            });
+
+            // Convert into a StablePointer<SharedTerm> by transmuting the pointer and adjusting the layout.
+            let ptr = slice_from_raw_parts_mut(result.ptr().as_ptr(), 0) as *mut SharedTerm;
+            (StablePointer::from_ptr(NonNull::new_unchecked(ptr)), inserted)
+        }
     }
 }
--- crates/aterm/src/storage/shared_term.rs
+++ crates/aterm/src/storage/shared_term.rs
@@ -42,10 +42,9 @@ impl Eq for SharedTerm {}
 unsafe impl SliceDst for SharedTerm {
     fn layout_for(len: usize) -> Result<Layout, LayoutError> {
         let header_layout = Layout::new::<SymbolRef<'static>>();
-        let annotated_layout = Layout::new::<bool>();
         let slice_layout = Layout::array::<ATermRef<'static>>(len)?;
 
-        repr_c(&[header_layout, annotated_layout, slice_layout])
+        repr_c(&[header_layout, slice_layout])
     }
 
     fn retype(ptr: std::ptr::NonNull<[()]>) -> NonNull<Self> {
@@ -100,30 +99,21 @@ impl SharedTerm {
 
     /// Returns the length for a [SharedTermLookup]
     pub(crate) fn length_for(object: &SharedTermLookup) -> usize {
-        object.arguments.len() + if object.annotation.is_some() { 1 } else { 0 }
+        object.arguments.len()
     }
 
     /// Constructs an uninitialised ptr from a [SharedTermLookup]
     pub(crate) unsafe fn construct(ptr: *mut SharedTerm, object: &SharedTermLookup) {
         let header_layout = Layout::new::<SymbolRef<'static>>();
-        let annotated_layout = Layout::new::<bool>();
         let slice_layout =
             Layout::array::<ATermRef<'static>>(object.arguments.len()).expect("Layout should not exceed isize");
 
-        let (header_layout, annotated_offset) = header_layout
-            .extend(annotated_layout)
-            .expect("Layout should not exceed isize");
         let (_, slice_offset) = header_layout
             .extend(slice_layout)
             .expect("Layout should not exceed isize");
-
         unsafe {
             ptr.cast::<SymbolRef<'static>>()
                 .write(SymbolRef::from_index(object.symbol.shared()));
-            let annotated = object.annotation.is_some();
-            ptr.byte_offset(annotated_offset as isize)
-                .cast::<bool>()
-                .write(annotated);
 
             for (index, argument) in object.arguments.iter().enumerate() {
                 ptr.byte_offset(slice_offset as isize)
@@ -147,7 +137,6 @@ impl Hash for SharedTerm {
 pub(crate) struct SharedTermLookup<'a> {
     pub(crate) symbol: SymbolRef<'a>,
     pub(crate) arguments: &'a [ATermRef<'a>],
-    pub(crate) annotation: Option<usize>,
 }
 
 impl Equivalent<SharedTerm> for SharedTermLookup<'_> {
@@ -161,7 +150,6 @@ impl Hash for SharedTermLookup<'_> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.symbol.hash(state);
         self.arguments.hash(state);
-        self.annotation.hash(state);
     }
 }
 
@@ -176,25 +164,26 @@ mod tests {
 
     use super::*;
 
-    // #[test]
-    // fn test_shared_symbol_size() {
-    //     // Cannot be a const assertion since the size depends on the length.
-    //     assert_eq!(
-    //         SharedTerm::layout_for(0)
-    //             .expect("The layout should not overflow")
-    //             .size(),
-    //         1 * std::mem::size_of::<usize>(),
-    //         "A SharedTerm without arguments should be the same size as the Symbol"
-    //     );
+    #[test]
+    #[cfg(not(debug_assertions))]
+    fn test_shared_symbol_size() {
+        // Cannot be a const assertion since the size depends on the length.
+        assert_eq!(
+            SharedTerm::layout_for(0)
+                .expect("The layout should not overflow")
+                .size(),
+            1 * std::mem::size_of::<usize>(),
+            "A SharedTerm without arguments should be the same size as the Symbol"
+        );
 
-    //     assert_eq!(
-    //         SharedTerm::layout_for(2)
-    //             .expect("The layout should not overflow")
-    //             .size(),
-    //         3 * std::mem::size_of::<usize>(),
-    //         "A SharedTerm with arity two should be the same size as the Symbol and two ATermRef arguments"
-    //     );
-    // }
+        assert_eq!(
+            SharedTerm::layout_for(2)
+                .expect("The layout should not overflow")
+                .size(),
+            3 * std::mem::size_of::<usize>(),
+            "A SharedTerm with arity two should be the same size as the Symbol and two ATermRef arguments"
+        );
+    }
 
     #[test]
     fn test_shared_term_lookup() {
@@ -205,7 +194,6 @@ mod tests {
         let lookup = SharedTermLookup {
             symbol: symbol.copy(),
             arguments: &[term.copy(), term.copy()],
-            annotation: None,
         };
 
         let ptr = Global.allocate_slice_dst(2).expect("Could not allocate slice dst");
