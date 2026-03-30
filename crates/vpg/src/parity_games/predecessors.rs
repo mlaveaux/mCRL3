cecbfa83e40e9d3f8fd6a39cdde14f9ce03e7fb3
Maurice Laveaux <m.laveaux@tue.nl>
2026-03-30 17:53:14 +0200

    Added various tests related to parity games.

--- crates/vpg/src/parity_games/predecessors.rs
+++ crates/vpg/src/parity_games/predecessors.rs
@@ -2,11 +2,11 @@
 
 use std::marker::PhantomData;
 
-use merc_collections::ByteCompressedVec;
 use merc_collections::bytevec;
+use merc_collections::ByteCompressedVec;
 
-use crate::PG;
 use crate::VertexIndex;
+use crate::PG;
 
 /// Stores the predecessors for a given parity game.
 pub struct Predecessors<'a> {
@@ -25,52 +25,85 @@ impl<'a> Predecessors<'a> {
     /// Creates the predecessors structure for the given parity game.
     pub fn new<G: PG>(game: &'a G) -> Self {
         let mut edges_from = bytevec![VertexIndex::new(0); game.num_of_edges()];
-        let mut state2incoming = bytevec![0; game.num_of_vertices()];
+        let mut vertex_to_predecessors = bytevec![0; game.num_of_vertices()];
 
-        // Count the number of incoming transitions for each state
-        for state_index in game.iter_vertices() {
-            for edge in game.outgoing_edges(state_index) {
-                state2incoming.update(edge.to().value(), |start| *start += 1);
+        // Count the number of incoming transitions for each vertex
+        for vertex_index in game.iter_vertices() {
+            for edge in game.outgoing_edges(vertex_index) {
+                vertex_to_predecessors.update(edge.to().value(), |start| *start += 1);
             }
         }
 
         // Compute the start offsets (prefix sum)
-        state2incoming.fold(0, |offset, start| {
+        vertex_to_predecessors.fold(0, |offset, start| {
             let new_offset = offset + *start;
             *start = offset;
             new_offset
         });
 
         // Place the transitions
-        for state_index in game.iter_vertices() {
-            for edge in game.outgoing_edges(state_index) {
-                state2incoming.update(edge.to().value(), |start| {
-                    edges_from.set(*start, state_index);
+        for vertex_index in game.iter_vertices() {
+            for edge in game.outgoing_edges(vertex_index) {
+                vertex_to_predecessors.update(edge.to().value(), |start| {
+                    edges_from.set(*start, vertex_index);
                     *start += 1;
                 });
             }
         }
 
-        state2incoming.fold(0, |previous, start| {
+        vertex_to_predecessors.fold(0, |previous, start| {
             let result = *start;
             *start = previous;
             result
         });
 
-        // Add sentinel state
-        state2incoming.push(edges_from.len());
+        // Add sentinel vertex
+        vertex_to_predecessors.push(edges_from.len());
 
         Self {
             edges_from,
-            vertex_to_predecessors: state2incoming,
+            vertex_to_predecessors,
             _marker: PhantomData,
         }
     }
 
     /// Returns an iterator over the predecessors the given vertex.
-    pub fn predecessors(&self, state_index: VertexIndex) -> impl Iterator<Item = VertexIndex> + '_ {
-        let start = self.vertex_to_predecessors.index(state_index.value());
-        let end = self.vertex_to_predecessors.index(state_index.value() + 1);
+    pub fn predecessors(&self, vertex_index: VertexIndex) -> impl Iterator<Item = VertexIndex> + '_ {
+        let start = self.vertex_to_predecessors.index(vertex_index.value());
+        let end = self.vertex_to_predecessors.index(vertex_index.value() + 1);
         (start..end).map(move |i| self.edges_from.index(i))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use merc_utilities::random_test;
+
+    use crate::{random_parity_game, Predecessors, PG};
+
+    #[test]
+    fn test_random_predecessors() {
+        random_test(100, |rng| {
+            let game = random_parity_game(rng, true, 50, 10, 5);
+
+            let predecessors = Predecessors::new(&game);
+
+            for vertex in game.iter_vertices() {
+                // Compute the expected predecessors by iterating over all vertices and their outgoing edges.
+                let expected_predecessors: Vec<_> = game
+                    .iter_vertices()
+                    .filter(|&v| game.outgoing_edges(v).any(|e| e.to() == vertex))
+                    .collect();
+                let actual_predecessors: Vec<_> = predecessors.predecessors(vertex).collect();
+                
+                assert_eq!(
+                    expected_predecessors.into_iter().sorted().collect::<Vec<_>>(),
+                    actual_predecessors.into_iter().sorted().collect::<Vec<_>>(),
+                    "Predecessors of vertex {} do not match",
+                    vertex.value()
+                );
+            }
+        })
     }
 }
