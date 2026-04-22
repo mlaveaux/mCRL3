9507be840bcb1c672a3346d037a99a073aa4f5ee
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-22 23:56:38 +0200

    Moved the generation of formulas to the syntax crate

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -3,16 +3,18 @@
 
 mod builder;
 mod consume;
+mod counterexample_formula;
 mod parse;
 mod precedence;
-mod syntax_tree;
 mod syntax_tree_display;
+mod syntax_tree;
 mod visitor;
 
 pub use builder::*;
 pub use consume::*;
+pub use counterexample_formula::*;
 pub use parse::*;
 pub use precedence::*;
-pub use syntax_tree::*;
 pub use syntax_tree_display::*;
+pub use syntax_tree::*;
 pub use visitor::*;
