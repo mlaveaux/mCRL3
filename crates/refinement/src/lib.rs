24eb5169fb63e0f963c92f908c704c060dcee517
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-22 23:57:16 +0200

    Smaller changes

--- crates/refinement/src/lib.rs
+++ crates/refinement/src/lib.rs
@@ -3,14 +3,12 @@
 
 mod antichain;
 mod counterexample_constructor;
-mod counterexample_formula;
 mod failures_refinement;
 mod impossible_futures;
 mod refinement;
 
 pub use antichain::*;
 pub use counterexample_constructor::*;
-pub use counterexample_formula::*;
 pub use failures_refinement::*;
 pub use impossible_futures::*;
 pub use refinement::*;
