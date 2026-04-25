cb565b253da63d8ab527bb8f7dd7e359e2c491b6
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-25 11:50:04 +0200

    Formatting

--- crates/lts/src/random_lts.rs
+++ crates/lts/src/random_lts.rs
@@ -144,3 +144,40 @@ pub fn mutate_lts<L: LTS, R: Rng>(
 
     Ok(builder.finish(lts.initial_state_index(), true))
 }
+
+#[cfg(kani)]
+mod tests {
+    use rand::TryRng;
+
+    use crate::random_lts;
+
+    /// A simple wrapper around `kani::any` to implement the `TryRng` trait, which is required by the random LTS generation functions.
+    struct ArbRng {}
+
+    impl TryRng for ArbRng {
+        type Error = std::convert::Infallible;
+
+        fn try_next_u32(&mut self) -> Result<u32, Self::Error> {
+            Ok(kani::any())
+        }
+
+        fn try_next_u64(&mut self) -> Result<u64, Self::Error> {
+            Ok(kani::any())
+        }
+
+        fn try_fill_bytes(&mut self, dst: &mut [u8]) -> Result<(), Self::Error> {
+            for byte in dst {
+                *byte = kani::any();
+            }
+
+            Ok(())
+        }
+    }
+
+    #[kani::proof]
+    #[kani::unwind(1)]
+    fn random_lts_test() {
+        let mut rng = ArbRng {};
+        let lts = random_lts(&mut rng, 0, 0, 0);
+    }
+}
