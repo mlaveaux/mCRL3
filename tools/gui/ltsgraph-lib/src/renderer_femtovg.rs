a7ba959676bca29a26c88681648f6a77f34291b7
Maurice Laveaux <m.laveaux@tue.nl>
2026-04-25 13:03:43 +0200

    Fixed compilation of GUI tools

--- tools/gui/ltsgraph-lib/src/renderer_femtovg.rs
+++ tools/gui/ltsgraph-lib/src/renderer_femtovg.rs
@@ -161,7 +161,7 @@ mod tests {
     #[test]
     fn test_femtovg_renderer() {
         let file = include_str!("../../../../examples/lts/abp.aut");
-        let lts = Arc::new(read_aut(file.as_bytes(), vec![]).unwrap());
+        let lts = Arc::new(read_aut(file.as_bytes()).unwrap());
 
         let mut canvas = Canvas::new(Void).unwrap();
         let viewer = Viewer::new(lts.clone());
