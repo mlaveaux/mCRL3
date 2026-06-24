1e7a714db9decc22a3ba1677b2bb59614df803ae
Maurice Laveaux <m.laveaux@tue.nl>
2026-06-24 23:42:20 +0200

    Print the metrics message without newline

--- crates/explore/src/cache_lps.rs
+++ crates/explore/src/cache_lps.rs
@@ -218,7 +218,7 @@ impl CacheMetrics {
 impl fmt::Display for CacheMetrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if cfg!(not(feature = "metrics")) {
-            return writeln!(f, "enable the 'metrics' feature to see cache metrics");
+            return write!(f, "enable the 'metrics' feature to see cache metrics");
         }
 
         writeln!(f, "summand cache metrics:")?;
