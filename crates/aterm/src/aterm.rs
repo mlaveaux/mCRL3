155c15fd9ba6924d0f884f866a7d70c498e5e912
Maurice Laveaux <m.laveaux@tue.nl>
2026-07-13 22:55:25 +0200

    Clarify documentation on thread-local destructors and safety in ATerm and GcMutex

--- crates/aterm/src/aterm.rs
+++ crates/aterm/src/aterm.rs
@@ -230,15 +230,17 @@ impl fmt::Debug for ATermRef<'_> {
 /// Note that terms use thread-local state for their protection mechanism, so
 /// [ATerm] is not [Send]. Moreover, this means that terms cannot be stored in
 /// thread-local storage themselves, or at least must be destroyed before the
-/// thread exits, because the order in which thread-local destructors are
-/// called is undefined. For this purpose one can use `ManuallyDrop` to simply
-/// never drop thread local terms, since exiting the thread will clean up the
-/// protection sets anyway.
+/// thread exits, because the order in which thread-local destructors are called
+/// is undefined, and as such a term could be destroyed after the thread-local
+/// term pool is destroyed, leading to undefined behavior.
+/// 
+/// For this purpose one can use `ManuallyDrop` to simply never drop thread
+/// local terms, since exiting the thread will clean up the protection sets
+/// anyway.
 ///
 /// We do not mark term access as unsafe, since that would make their use
-/// cumbersome. An alternative would be to require
-/// THREAD_TERM_POOL.with(|tp| ...) around every access, but that would
-/// be very verbose.
+/// cumbersome. An alternative would be to require `THREAD_TERM_POOL.with(|tp|
+/// ...)` around every access, but that would be very verbose.
 pub struct ATerm {
     term: ATermRef<'static>,
 
