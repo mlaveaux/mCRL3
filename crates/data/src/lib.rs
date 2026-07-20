d68326ac72376629743ce6de9ab16d88055907e5
Maurice Laveaux <m.laveaux@tue.nl>
2026-07-20 12:16:06 +0200

    Ensure that the Term variant is also marked properly

--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -3,6 +3,7 @@
 
 mod data_expression;
 mod data_terms;
+mod machine_word_evaluation;
 mod mcrl2_data_specification;
 mod sort_terms;
 
@@ -22,6 +23,7 @@ pub use data_expression::DataVariableRef;
 pub use data_expression::DataWhereClause;
 pub use data_expression::DataWhrDecl;
 pub use data_expression::MachineNumber;
+pub use data_expression::MachineNumberRef;
 pub use data_expression::to_untyped_data_expression;
 pub use data_terms::is_container_sort;
 pub use data_terms::is_data_application;
@@ -31,6 +33,7 @@ pub use data_terms::is_data_machine_number;
 pub use data_terms::is_data_variable;
 pub use data_terms::is_data_where_clause;
 pub use data_terms::is_function_sort;
+pub use machine_word_evaluation::try_evaluate_machine_word;
 pub use mcrl2_data_specification::Mcrl2DataSpecification;
 pub use sort_terms::BasicSort;
 pub use sort_terms::BasicSortRef;
