bfa60c9dbe802ab76044b9081170a2a2113667ab
Maurice Laveaux <m.laveaux@tue.nl>
2026-08-07 16:12:11 +0200

    Removed various glob exports

--- tools/mcrl2/crates/mcrl2/src/atermpp/mod.rs
+++ tools/mcrl2/crates/mcrl2/src/atermpp/mod.rs
@@ -11,15 +11,39 @@ mod random_term;
 mod symbol;
 mod thread_aterm_pool;
 
-pub use aterm::*;
-pub use aterm_int::*;
-pub use aterm_list::*;
-pub use aterm_string::*;
-pub(crate) use busy_forbidden::*;
-pub(crate) use convert::mcrl2_aterm_to_merc;
-pub use convert::merc_aterm_to_mcrl2;
-pub use markable::*;
-pub use protected::*;
-pub(crate) use random_term::*;
-pub use symbol::*;
-pub(crate) use thread_aterm_pool::*;
+pub use aterm::ATerm;
+pub use aterm::ATermArgs;
+pub use aterm::ATermRef;
+pub use aterm::ATermSend;
+pub use aterm::TermIterator;
+
+pub use aterm_int::ATermInt;
+pub use aterm_int::ATermIntRef;
+pub use aterm_int::is_aterm_int;
+
+pub use aterm_list::ATermList;
+pub use aterm_list::ATermListIter;
+pub use aterm_list::ATermListIterRef;
+pub use aterm_list::ATermListRef;
+
+pub use aterm_string::ATermString;
+pub use aterm_string::ATermStringRef;
+pub use aterm_string::is_aterm_string;
+
+pub use busy_forbidden::BfTermPool;
+pub use busy_forbidden::BfTermPoolRead;
+pub use busy_forbidden::BfTermPoolThreadWrite;
+pub use busy_forbidden::BfTermPoolWrite;
+
+pub use markable::Markable;
+pub use markable::Todo;
+
+pub use protected::Protected;
+
+pub use random_term::random_term;
+
+pub use symbol::Symbol;
+pub use symbol::SymbolRef;
+
+pub use thread_aterm_pool::ThreadTermPool;
+pub(crate) use thread_aterm_pool::THREAD_TERM_POOL;
